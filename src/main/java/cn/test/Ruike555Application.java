03c36eaca838cba43c0e9945056d51a9bfd85d91
WDLJQ <132349679+WDLJQ@users.noreply.github.com>
2024-07-05 16:21:55 +0800

    first commit

---
+++ src/main/java/cn/test/Ruike555Application.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruike555Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ruike555Application.class, args);
+    }
+
+}
