ca17373872e14f71745dde3de970a05c68ff3f5f
WDLJQ <132349679+WDLJQ@users.noreply.github.com>
2024-07-05 16:52:15 +0800

    Create user.java

---
+++ src/main/java/cn/test/entity/user.java
@@ -0,0 +1,11 @@
+package cn.test.entity;
+
+public class user {
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+    private Integer age;
+    private String email;
+
+}
